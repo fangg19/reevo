1f069c63fbb39d7cd045bc91e4db42435367e473
Șerban Alexandru Jonny <alexjohnny6@gmail.com>
2021-08-22 22:36:02 +0300

    moving graphics into sprites. added interaction on generated sprites

--- app.js
+++ app.js
@@ -4,43 +4,76 @@ const app = new Application({
   width: 1000,
   height: 700,
   backgroundColor: 0xfbda66,
+  antialias: true,
 });
 
-document.body.appendChild(app.view);
+app.renderer.view.style.position = 'absolute';
+app.renderer.view.style.display = 'block';
 
-const container = new PIXI.Container();
+let shapesCounter = document.getElementById('shapes-counter');
+let occupiedArea = document.getElementById('occupied-area');
+
+const canvas = document.querySelector('#canvas-wrapper').appendChild(app.view);
+
+//generating graphics and turning them into sprites
 const Graphics = PIXI.Graphics;
+const rectangle = new Graphics();
 
-const randomX = Math.random() * app.screen.width;
-const randomY = Math.random() * app.screen.height;
+let texture = app.renderer.generateTexture(rectangle);
+let sprite = new PIXI.Sprite(texture);
+
+function destroy() {
+  sprite.destroy(true);
+}
+
+sprite.interactive = true;
+sprite.buttonMode = true;
+sprite.on('pointerdown', destroy);
+
+let shapes = [];
 
 //get mouse coordinates
-let mouseX = document.body.addEventListener('mousemove', (event) => {
-  mouseX = event.x;
-});
+let mousePosition;
+function getMousePos(canvas, evt) {
+  const rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top,
+  };
+}
+
+canvas.addEventListener(
+  'mousemove',
+  function (evt) {
+    let mousePos = getMousePos(canvas, evt);
+    mousePosition = mousePos;
+  },
+  false
+);
 
 //create new shape
 const createShape = () => {
-  const triangle = new Graphics();
-
-  triangle
+  rectangle
     .beginFill(0x4361ee)
-    .drawPolygon([400, 50, 350, 150, 450, 120])
+    .drawRect(mousePosition.x - 100 / 2, 0, 100, 100)
     .endFill();
+  shapes.push(sprite);
 
-  container.addChild(triangle);
-  app.stage.addChild(container);
+  sprite.addChild(rectangle);
+  app.stage.addChild(sprite);
 
-  container.y = 0;
-  container.x = mouseX;
-  console.log(mouseX);
+  console.log(sprite);
+  console.log(shapes);
+
+  shapesCounter.innerText = `No. of current shapes: ${shapes.length}`;
+  occupiedArea.innerText = `10% of the area is occupied.`;
 };
 
 //onclick call createShape
-document.body.addEventListener('click', createShape);
+canvas.addEventListener('pointerdown', createShape);
 
 //ticker for shape movement
 app.ticker.add((delta) => loop(delta));
-function loop(delta) {
-  container.y += 1;
-}
+const loop = (delta) => {
+  // rectangle.y += 1;
+};

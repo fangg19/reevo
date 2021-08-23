d401ba2f7ae33d7a4baeae0d9af7c5da9fb733de
Șerban Alexandru Jonny <alexjohnny6@gmail.com>
2021-08-23 23:24:05 +0300

    working create shape function. added setInterval and gravity controler

--- app.js
+++ app.js
@@ -6,31 +6,21 @@ const app = new Application({
   backgroundColor: 0xfbda66,
   antialias: true,
 });
-
 app.renderer.view.style.position = 'absolute';
 app.renderer.view.style.display = 'block';
 
 let shapesCounter = document.getElementById('shapes-counter');
 let occupiedArea = document.getElementById('occupied-area');
 
+let shapesPerSecond = document.getElementById('shapes-per-second');
+let gravityValue = document.getElementById('gravity-value');
+
 const canvas = document.querySelector('#canvas-wrapper').appendChild(app.view);
 
-//generating graphics and turning them into sprites
-const Graphics = PIXI.Graphics;
-const rectangle = new Graphics();
-
-let texture = app.renderer.generateTexture(rectangle);
-let sprite = new PIXI.Sprite(texture);
-
-function destroy() {
-  sprite.destroy(true);
-}
-
-sprite.interactive = true;
-sprite.buttonMode = true;
-sprite.on('pointerdown', destroy);
-
 let shapes = [];
+let speed = 5;
+
+let area = 0;
 
 //get mouse coordinates
 let mousePosition;
@@ -41,7 +31,6 @@ function getMousePos(canvas, evt) {
     y: evt.clientY - rect.top,
   };
 }
-
 canvas.addEventListener(
   'mousemove',
   function (evt) {
@@ -52,28 +41,65 @@ canvas.addEventListener(
 );
 
 //create new shape
-const createShape = () => {
-  rectangle
-    .beginFill(0x4361ee)
-    .drawRect(mousePosition.x - 100 / 2, 0, 100, 100)
-    .endFill();
-  shapes.push(sprite);
+const createShape = (xPosition) => {
+  const Graphics = PIXI.Graphics;
+  const shape = new Graphics();
 
-  sprite.addChild(rectangle);
+  let texture = app.renderer.generateTexture(shape);
+  let sprite = new PIXI.Sprite(texture);
+
+  shape
+    .beginFill(0x4361ee)
+    .drawRect(xPosition - 100 / 2, -100, 100, 100)
+    .endFill();
+
+  sprite.interactive = true;
+  sprite.buttonMode = true;
+  sprite.speed = speed;
+
+  shapes.push(sprite);
+  sprite.addChild(shape);
   app.stage.addChild(sprite);
 
-  console.log(sprite);
   console.log(shapes);
 
-  shapesCounter.innerText = `No. of current shapes: ${shapes.length}`;
-  occupiedArea.innerText = `10% of the area is occupied.`;
+  return sprite;
 };
 
 //onclick call createShape
-canvas.addEventListener('pointerdown', createShape);
+window.app = app;
+app.renderer.plugins.interaction.on('pointerdown', () => {
+  createShape(mousePosition.x);
+});
 
 //ticker for shape movement
-app.ticker.add((delta) => loop(delta));
-const loop = (delta) => {
-  // rectangle.y += 1;
-};
+app.ticker.add(updateShapes);
+
+function updateShapes(delta) {
+  //move shapes and clear them from stage and array after getting out of stage
+  for (let i = 0; i < shapes.length; i++) {
+    shapes[i].position.y += shapes[i].speed;
+    area =
+      shapes[i].children[0].width *
+      shapes[i].children[0].height *
+      shapes.length;
+
+    if (shapes[i].position.y > app.screen.height) {
+      shapes[i].dead = true;
+    }
+    if (shapes[i].dead) {
+      app.stage.removeChild(shapes[i]);
+      shapes.splice(i, 1);
+      area = 0;
+    }
+  }
+  //keep the info updated
+
+  speed = Number(gravityValue.value) || 5;
+  shapesCounter.innerText = `No. of current shapes on the screen: ${shapes.length}`;
+  occupiedArea.innerText = `${area}p^2 of the area is occupied by the shapes.`;
+}
+
+const shapesInterval = setInterval(() => {
+  createShape(Math.random() * app.screen.width);
+}, 1000);
